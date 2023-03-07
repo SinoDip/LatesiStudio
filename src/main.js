169f5798b5b33c059379742d658e032d9be879cd
Sino Samateh <sinosamateh@gmail.com>
2023-03-07 17:15:32 +0100

    first coding //  07/03/2023 17:15

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import "./styles/style.css";
 import "./styles/reset.css";
+import "./styles/variables.css";
+
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const menu = document.querySelector(".menu");
+
+hamburgerIcon.addEventListener("click", () => {
+  menu.classList.toggle("show");
+  hamburgerIcon.classList.toggle("close");
+});
+
+console.log("test");
