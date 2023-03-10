74cb83891ddbb128a027f5566ceeed5ce1740928
Sino Samateh <sinosamateh@gmail.com>
2023-03-10 03:35:27 +0100

    tried to fix nav

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import "./styles/style.css";
 import "./styles/reset.css";
 import "./styles/variables.css";
-
-const hamburgerIcon = document.getElementById("hamburger-icon");
-
-hamburgerIcon.addEventListener("click", function () {
-  hamburgerIcon.classList.toggle("change");
-  console.log("meni");
-});
+import "./styles/nav.css";
